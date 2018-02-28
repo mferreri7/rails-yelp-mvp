42047cb38b03abcc80d5219e2a6f4e7a6337032b
Miguel Ferrer <miguef7@gmail.com>
2018-02-28 15:46:19 +0100

    yelp mvp-done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "Vamonos!!!!!"
+
+10.times do
+  restaurant = Restaurant.new(name:Faker::MostInterestingManInTheWorld.quote ,address:Faker::Address.street_address, phone_number:Faker::PhoneNumber.cell_phone, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  5.times do
+    restaurant.reviews << Review.new(content: Faker::Hacker.say_something_smart, rating: [0,1,2,3,4,5].sample)
+  end
+  restaurant.save!
+end
+
+p "Done!"
+
